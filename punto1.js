74fc1a01914af5f219e7e31d83316d24aff5582c
BillJean521 <jean.cortina.ttc.998@unilibre.edu.co>
2024-11-14 19:51:07 -0500

    cambio3

--- punto1.js
+++ punto1.js
@@ -8,4 +8,5 @@ for(i=0;i<numeros.length();i++){
 }
 
 promedio=suma/numeros.length();
-document.getElementById("resultado").value = suma;
+document.getElementById("resultado").innerHTML = suma;
+document.getElementById("promedio").innerText = promedio;
