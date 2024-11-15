741ec07e376fda8570501d497f1cde8a3f3c359a
BillJean521 <jean.cortina.ttc.998@unilibre.edu.co>
2024-11-14 19:46:57 -0500

    jscs

--- punto1.js
+++ punto1.js
@@ -3,8 +3,9 @@ let suma=0;
 let promedio=0;
 let i=0;
 
-for(i=0;i<numeros.length;i++){
+for(i=0;i<numeros.length();i++){
     suma += numeros[i];
 }
 
+promedio=suma/numeros.length();
 document.getElementById("resultado").value = suma;
